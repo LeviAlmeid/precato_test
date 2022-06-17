a5389c7360b5f6c1a8dcb48e933128cd698208d4
Levi Almeida <levialmeida02@outlook.com>
2022-06-17 20:36:28 -0300

    tirando connection

--- src/repositories/subscriptionsRepository.ts
+++ src/repositories/subscriptionsRepository.ts
@@ -1,5 +1,5 @@
 import { subsciptions } from "../model/subscription";
-const mysql = require('../../mysql')
+const mysql = require('../../mysql');
 
 interface ICreateSubscriptionsDTO {
     name: string
@@ -27,17 +27,17 @@ class SubscriptionsRepository {
         })
 
 
-        mysql.getConnection((error, conn) =>{
-            conn.query('INSERT INTO subscription (id, name, date, last_message, email, active) VALUES (?, ?, ?, ?, ?, ?)'),
-            [sub.id, sub.name, sub.subscription_date, sub.last_message, sub.email, sub.active],
-            (error, result, field) =>{
-                conn.release();
+        // mysql.getConnection((error, conn) =>{
+        //     conn.query('INSERT INTO subscription (id,  name, date, last_message, email, active) VALUES (?, ?, ?, ?, ?, ?)'),
+        //     [sub.id, sub.name, sub.subscription_date, sub.last_message, sub.email, sub.active],
+        //     (error, result, field) =>{
+        //         conn.release();
 
-                if(error){
-                    throw new Error("Erro ao inserir dados")
-                }
-            }
-        })
+        //         if(error){
+        //             throw new Error("Erro ao inserir dados")
+        //         }
+        //     }
+        // })
 
 
         this.subscriptions.push(sub)
